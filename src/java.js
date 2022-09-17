bfc3e1aea4296744eb0cc4d875f00cf339c935ff
Arinuccia <yastremskaya.01@gmail.com>
2022-09-17 20:43:52 +0300

    added an api

--- src/java.js
+++ src/java.js
@@ -31,7 +31,8 @@ function displayForecast(response) {
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
-  let apiUrl = "";
+  let apiUrl =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}";
   axios.get(apiUrl).then(displayForecast);
 }
 
