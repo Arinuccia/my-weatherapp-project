89b0ee6c1b5ac59302dcef53e359a688d8313011
Arinuccia <yastremskaya.01@gmail.com>
2023-09-25 17:42:57 +0300

    added a new property

--- src/java.js
+++ src/java.js
@@ -81,7 +81,8 @@ function showTemperature(response) {
   if (
     response.data.weather[0].description == "scattered clouds" ||
     response.data.weather[0].description == "broken clouds" ||
-    response.data.weather[0].description == "mist"
+    response.data.weather[0].description == "mist" ||
+    response.data.weather[0].description == "light rain"
   ) {
     document.body.style.background =
       "linear-gradient(to right, #d7d2cc 0%, #304352 100%)";
